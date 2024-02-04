e9ee02aa16661ed4e21541fabdae106846ef8d23
Ashraful Islam Emon <emon118.bd@gmail.com>
2024-02-05 00:15:24 +0600

    Add files via upload

---
+++ 3_10 logical variables and operators.R
@@ -0,0 +1,30 @@
+#logical
+# TRUE T
+# FALSE F
+
+4<5
+5>19
+4==4
+# == 
+# != 
+# < 
+# > 
+# <= 
+# >= 
+# ! 
+# | 
+# & 
+# isTRUE(...)
+
+result= 8>6
+result
+
+result2 = !(5>4)
+result2
+
+result | result2
+result & result2
+
+isTRUE(result)
+isTRUE(result2)
+
---
+++ 3_11 while loop.R
@@ -0,0 +1,11 @@
+
+while( TRUE){
+  print("emon")
+}
+# press esc or stop
+
+i=1
+while(i<=10){
+  print(i)
+  i=i+1
+}
