464b4ef7cc23ae44cee37f3fdbece0f33b4ad3b3
Ashraful Islam Emon <emon118.bd@gmail.com>
2024-02-07 02:27:52 +0600

    Add files via upload

---
+++ 4_ 19 vector.R
@@ -0,0 +1,32 @@
+
+vc= c(3,4,5)
+typeof(vc)
+is.numeric(vc)
+is.integer(vc)
+is.double(vc)
+
+vc = c(3L,3L,2L)
+is.numeric(vc)
+is.integer(vc)
+is.double(vc)
+
+vc= c("sf","waesfa")
+is.numeric(vc)
+is.integer(vc)
+is.double(vc)
+is.character(vc)
+
+vc= c("sf","waesfa",34)
+is.numeric(vc)
+is.integer(vc)
+is.double(vc)
+is.character(vc)
+
+x= seq(0,15,4)
+x
+
+y= rep("sd ",4)
+y
+
+z= rep(32, 5)
+z
